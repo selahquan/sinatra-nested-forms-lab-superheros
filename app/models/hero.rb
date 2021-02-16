30fd41cfa70751a4e6f32481b959a1d4459a69c8
Josh <josh.handon@gmail.com>
2021-02-16 11:29:03 -0500

    commit tree 6fe2ab8a936a2ca0ec3894c587fc48792718eecd

---
+++ app/models/hero.rb
@@ -0,0 +1,10 @@
+class Heroes
+    attr_reader :name, :power, :bio
+
+    def initialize(name, power, bio)
+        @name = name
+        @power = power
+        @bio = bio
+    end
+
+end
---
+++ app/models/team.rb
@@ -0,0 +1,9 @@
+class Team
+    attr_reader :name, :motto
+
+    def initialize(name, motto)
+        @name = name
+        @motto = motto
+    end
+    
+end
